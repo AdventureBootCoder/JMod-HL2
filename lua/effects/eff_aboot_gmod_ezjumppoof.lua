5af11ffff0f04c1d570f25765782f553c0672b4b
AdventureBootCoder <96029388+AdventureBootCoder@users.noreply.github.com>
2023-06-02 21:14:03 -0700

    Fixed model move problems

---
+++ lua/effects/eff_aboot_gmod_ezjumppoof.lua
@@ -0,0 +1,44 @@
+﻿-- Jackarunda 2021
+--,"particles/smokey","particle/particle_smokegrenade","sprites/mat_jack_smoke1","sprites/mat_jack_smoke2","sprites/mat_jack_smoke3"}
+local Sprites = {"particle/smokestack"}
+
+function EFFECT:Init(data)
+	local Pos, Norm, Vel, Scl = data:GetOrigin(), data:GetNormal(), data:GetStart(), data:GetScale()
+	local R, G, B = 255, 0, 0
+	local Emitter = ParticleEmitter(Pos)
+	local Sprite = Sprites[math.random(1, #Sprites)]
+
+	for i = 1, 2 * Scl do
+		local RollParticle = Emitter:Add(Sprite, Pos)
+
+		if RollParticle then
+			RollParticle:SetVelocity(Vel + Norm)
+			RollParticle:SetAirResistance(100)
+			RollParticle:SetDieTime(math.Rand(2, 12))
+			RollParticle:SetStartAlpha(math.random(150, 200))
+			RollParticle:SetEndAlpha(0)
+			local Size = math.Rand(20, 40)
+			RollParticle:SetStartSize(Size / 20)
+			RollParticle:SetEndSize(Size * 4)
+			RollParticle:SetRoll(math.Rand(-3, 3))
+			RollParticle:SetRollDelta(math.Rand(-2, 2))
+			local Vec = VectorRand() * 10
+			RollParticle:SetGravity(Vec)
+			RollParticle:SetLighting(false)
+			local Brightness = math.Rand(.8, 1)
+			RollParticle:SetColor(R * Brightness, G * Brightness, B * Brightness)
+			RollParticle:SetCollide(true)
+			RollParticle:SetBounce(1)
+		end
+	end
+
+	Emitter:Finish()
+end
+
+function EFFECT:Think()
+	return false
+end
+
+function EFFECT:Render()
+end
+-- no u
