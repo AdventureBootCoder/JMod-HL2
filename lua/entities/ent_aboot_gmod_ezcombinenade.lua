d41bc31811c32bd42a0e3604d7b13760e4239e80
AdventureBootCoder <96029388+AdventureBootCoder@users.noreply.github.com>
2023-10-02 22:50:08 -0700

    Combine grenade

--- lua/entities/ent_aboot_gmod_ezcombinenade.lua
+++ lua/entities/ent_aboot_gmod_ezcombinenade.lua
@@ -1,46 +1,56 @@
 -- AdventureBoots 2023
 AddCSLuaFile()
-ENT.Base="ent_jack_gmod_ezgrenade"
-ENT.Author="AdventureBoots"
-ENT.PrintName="EZ Combine Grenade"
-ENT.Category="JMod - EZ HL:2"
-ENT.Spawnable=false
-ENT.JModPreferredCarryAngles=Angle(0,100,0)
-ENT.Model="models/weapons/w_grenade.mdl"
+ENT.Base = "ent_jack_gmod_ezgrenade"
+ENT.Author = "AdventureBoots"
+ENT.PrintName = "EZ Combine Grenade"
+ENT.Category = "JMod - EZ HL:2"
+ENT.Spawnable = true
+---
+ENT.JModPreferredCarryAngles = Angle(0, 100, 0)
+ENT.Model= "models/weapons/w_npcnade.mdl"
 --ENT.Material=""
-ENT.HardThrowStr=350
-ENT.SoftThrowStr=250
-ENT.SpoonScale=2
+ENT.HardThrowStr = 350
+ENT.SoftThrowStr = 250
+ENT.SpoonScale = 2
+
 if(SERVER)then
 	function ENT:Prime()
 		self:SetState(JMod.EZ_STATE_PRIMED)
 		self:EmitSound("weapons/pinpull.wav", 60, 100)
-		self:SetBodygroup(3, 1)
+		--self:SetBodygroup(3, 1)
 	end
+
 	function ENT:Arm()
-		self:SetBodygroup(2, 1)
+		--self:SetBodygroup(2, 1)
 		self:SetState(JMod.EZ_STATE_ARMED)
 		self:SpoonEffect()
+		if math.random(1, 1000) == 1 then
+			util.SpriteTrail( self, 1, Color( 255, 0, 0 ), false, 3, 1, 1, 1 / ( 3 + 1 ) * 0.5, "trails/lol" )
+		else
+			util.SpriteTrail( self, 1, Color( 255, 0, 0 ), false, 2, 0.1, 1, 1 / ( 2 + 0.1 ) * 0.5, "trails/plasma" )
+		end
 		timer.Simple(4, function()
 			if(IsValid(self))then self:Detonate() end
 		end)
 	end
+
 	function ENT:Detonate()
 		if(self.Exploded)then return end
-		self.Exploded=true
-		local SelfPos=self:GetPos()
-		local PowerMult=0.69
-		JMod.Sploom(self.Owner,self:GetPos(),150,400)
+		self.Exploded = true
+		local SelfPos = self:GetPos()
+		local PowerMult = 0.69
+		JMod.Sploom(self.Owner, self:GetPos(), 150, 400)
 		JMod.BlastDoors(self.Owner, self:GetPos(), 100, 445, false)
-		self:EmitSound("snd_jack_fragsplodeclose.wav",90,100)
-		local plooie=EffectData()
+		self:EmitSound("snd_jack_fragsplodeclose.wav", 90, 100)
+		local plooie = EffectData()
 		plooie:SetOrigin(SelfPos)
 		plooie:SetScale(0.5)
-		util.Effect("eff_jack_plastisplosion",plooie,true,true)
-		util.ScreenShake(SelfPos,99999,99999,1,750*1.75)
-		local OnGround=util.QuickTrace(SelfPos+Vector(0,0,5),Vector(0,0,-15),{self}).Hit
+		util.Effect("eff_jack_plastisplosion", plooie, true, true)
+		util.ScreenShake(SelfPos, 99999, 99999, 1, 750 * 1.75)
+		local OnGround = util.QuickTrace(SelfPos+Vector(0, 0, 5),Vector(0, 0, -15), {self}).Hit
 		self:Remove()
 	end
+
 	function ENT:OnTakeDamage(dmginfo)
 		if self.Exploded then return end
 		if dmginfo:GetInflictor() == self then return end
@@ -56,9 +66,39 @@ if(SERVER)then
 			end
 		end
 	end
+
 elseif(CLIENT)then
-	function ENT:Draw()
-		self:DrawModel()
+	function ENT:Initialize()
+		self.Spoon = JMod.MakeModel(self, "models/jmod/explosives/grenades/firenade/incendiary_grenade_spoon.mdl")
 	end
-	language.Add("ent_aboot_gmod_ezcombinenade","EZ Standered Overwatch Grenade")
+
+	function ENT:Draw()
+		local State = self:GetState()
+		self:DrawModel()
+		if State ~= JMod.EZ_STATE_ARMED then
+			local SpoonAng = self:GetAngles()
+			JMod.RenderModel(self.Spoon, self:GetPos(), SpoonAng)
+		end
+	end
+
+	function ENT:Think()
+		local State, Pos, Up = self:GetState(), self:GetPos(), self:GetUp()
+		if State == JMod.EZ_STATE_ARMED then
+			local DLight = DynamicLight(self:EntIndex())
+
+			if DLight then
+				DLight.Pos = Pos + Up * 5 + Vector(0, 0, 10)
+				DLight.r = 255
+				DLight.g = 0
+				DLight.b = 0
+				DLight.Brightness = 1
+				DLight.Size = 80
+				DLight.Decay = 15000
+				DLight.DieTime = CurTime() + .3
+				DLight.Style = 0
+			end
+		end
+	end
+
+	language.Add("ent_aboot_gmod_ezcombinenade","EZ Combine Grenade")
 end
