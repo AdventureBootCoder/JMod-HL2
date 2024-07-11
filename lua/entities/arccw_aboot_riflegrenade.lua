71fcaf0162e3558f59a24639c13f10906ca29130
AdventureBootCoder <96029388+AdventureBootCoder@users.noreply.github.com>
2024-07-10 19:05:48 -0700

    Messing with frags

--- lua/entities/arccw_aboot_riflegrenade.lua
+++ lua/entities/arccw_aboot_riflegrenade.lua
@@ -81,7 +81,7 @@ function ENT:DoDetonation()
 	local attacker = IsValid(self:GetOwner()) and self:GetOwner() or self
 	local SelfPos = self:GetPos()
 	if self.EZfragment then
-		JMod.FragSplosion(self, SelfPos, 600, 20, 2000, attacker, self:GetVelocity():GetNormalized(), 0.3)
+		JMod.FragSplosion(self, SelfPos, 500, 20, 1000, attacker, self:GetVelocity():GetNormalized(), 0.6)
 		local Dmg = DamageInfo()
 		Dmg:SetAttacker(attacker)
 		Dmg:SetInflictor(self)
@@ -91,7 +91,7 @@ function ENT:DoDetonation()
 		for _, ent in ipairs(ents.FindInCone(SelfPos, self.GrenadeDir, self.GrenadeRadius * 1.5, 0.5)) do
 			if IsValid(ent) and (ent ~= self) and JMod.VisCheck(SelfPos, ent:GetPos(), self) then
 				Dmg:SetDamage((self.GrenadeDamage or self.Damage or 10) / (SelfPos:Distance(ent:GetPos()) / 1000))
-				ent:TakeDamageInfo(Dmg)
+				--ent:TakeDamageInfo(Dmg)
 			end
 		end
 	end
