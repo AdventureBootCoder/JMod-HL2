27fe97ea8eb74ec82d4c555a8e393ae76e2b4051
AdventureBootCoder <96029388+AdventureBootCoder@users.noreply.github.com>
2025-07-11 12:18:30 -0700

    Use global blur background function

--- lua/entities/ent_aboot_gmod_ezshippingcontainer.lua
+++ lua/entities/ent_aboot_gmod_ezshippingcontainer.lua
@@ -269,34 +269,6 @@ elseif CLIENT then
 		end
 	end
 
-	local blurMat = Material("pp/blurscreen")
-	local Dynamic = 0
-	local function BlurBackground(panel)
-		if not (IsValid(panel) and panel:IsVisible()) then return end
-		local layers, density, alpha = 1, 1, 255
-		local x, y = panel:LocalToScreen(0, 0)
-		local FrameRate, Num, Dark = 1 / FrameTime(), 5, 150
-	
-		if GetConVar("jmod_cl_blurry_menus"):GetBool() then
-			surface.SetDrawColor(255, 255, 255, alpha)
-			surface.SetMaterial(blurMat)
-	
-			for i = 1, Num do
-				blurMat:SetFloat("$blur", (i / layers) * density * Dynamic)
-				blurMat:Recompute()
-				render.UpdateScreenEffectTexture()
-				surface.DrawTexturedRect(-x, -y, ScrW(), ScrH())
-			end
-	
-			surface.SetDrawColor(0, 0, 0, Dark * Dynamic)
-			surface.DrawRect(0, 0, panel:GetWide(), panel:GetTall())
-			Dynamic = math.Clamp(Dynamic + (1 / FrameRate) * 7, 0, 1)
-		else
-			surface.SetDrawColor(0, 0, 0, 180)
-			surface.DrawRect(0, 0, panel:GetWide(), panel:GetTall())
-		end
-	end
-
 	net.Receive("ABoot_ContainerMenu", function() 
 		local Menu = net.ReadBool()
 		local Container = net.ReadEntity()
@@ -341,7 +313,7 @@ elseif CLIENT then
 				surface.PlaySound("snds_jack_gmod/ez_gui/menu_open.ogg")
 			end
 	
-			BlurBackground(self)
+			EZBlurBackground(self)
 		end
 
 		MotherFrame:MakePopup()
